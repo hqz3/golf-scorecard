948322b851135a7bc01c05a16fe3b9b933a3fa89
hqz3 <heqzheng@gmail.com>
2024-05-14 17:30:32 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <header className="font-bold">GOLF SCORECARD</header>
+    </div>
+  );
+}
+
+export default App;
